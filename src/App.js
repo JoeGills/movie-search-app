dca0ee798ac96cfe943008b2b4deea22069d86ef
Joseph Gills <joseph.gills@hyperisland.se>
2023-05-25 10:53:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SearchForm from "./components/SearchForm";
+import MovieDetails from "./components/MovieDetails";
 
 function App() {
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const handleSearch = (searchTerm) => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=d35f2c9a120471ee44fc42bdfd548eff&query=${searchTerm}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results && data.results.length > 0) {
+          setSelectedMovie(data.results[0]);
+        } else {
+          setSelectedMovie(null);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching movie:", error);
+        setSelectedMovie(null);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Movie Search App</h1>
+      <SearchForm onSearch={handleSearch} />
+      {selectedMovie && <MovieDetails movie={selectedMovie} />}
     </div>
   );
 }
